e6d33ff76d29f8af357495094de59781c2274f55
claudiavidal <claudiavidalpujol@hotmail.com>
2016-02-24 15:43:56 +0100

    :)

--- snake.c
+++ snake.c
@@ -2,21 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /*ajout element a la tete du snake*/
 
-snake cons(snake s, position p){
-snake S;
-S = malloc(sizeof(struct maillon));
-S->coordonnees = p;
-S->suivant= s;
-return(p);
+serpent cons(serpent s, position p){
+    serpent S;
+    S = malloc(sizeof(struct serpent));
+    S->coordonnees = p;
+    S->suivant= s;
+return(s);
 }
 
 /*eliminer le dernier element du snake*/
 
-snake eliminer(snake s){
-    snake I = NULL;
-    snake S = NULL;
+serpent eliminer(serpent s){
+    serpent I = NULL;
+    serpent S = NULL;
     while (s->suivant != NULL){
         I = cons(I,s->coordonnees);
     }
@@ -28,35 +29,31 @@ return S;
 
 /*deplacement*/
 
-snake deplacement(int d, snake s){ /*2,4,6 et 8 a remplacer pas las fleches*/
+serpent deplacement(int d, serpent s){ /*2,4,6 et 8 a remplacer pas las fleches*/
 s = eliminer(s);
 position p;
+position c;
 c = s->coordonnees;
-    if (d=2){
-        p.x = c.x;
-        p.y = c.y - 1;
+    if (d==2){
+        p->x = c->x;
+        p->y = c->y - 1;
         s = cons(s,p);
     }
-    if (d=8){
-        p.x = c.x;
-        p.y = c.y + 1;
+    if (d==8){
+        p->x = c->x;
+        p->y = c->y + 1;
         s = cons(s,p);
     }
-    if (d=6){
-        p.x = c.x + 1;
-        p.y = c.y;
+    if (d==6){
+        p->x = c->x + 1;
+        p->y = c->y;
         s = cons(s,p);
     }
-    if (d=4){
-        p.x = c.x - 1;
-        p.y = c.y;
+    if (d==4){
+        p->x = c->x - 1;
+        p->y = c->y;
         s = cons(s,p);
     }
 return s;
 }
-    
-    
-    
-    
-    
-        
+
