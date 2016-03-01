5c86d460019089dd017edf72fe50f8f524f70063
Yoann89 <yoann.calonnec@orange.fr>
2016-03-01 09:48:47 +0100

    Modification structure du snake

--- Code/snake.c
+++ Code/snake.c
@@ -5,55 +5,54 @@
 
 /*ajout element a la tete du snake*/
 
-serpent cons(serpent s, position p){
+void ajout_entete(serpent s, position p){
     serpent S;
     S = malloc(sizeof(struct serpent));
     S->coordonnees = p;
     S->suivant= s;
-return(s);
+return(S);
 }
 
 /*eliminer le dernier element du snake*/
 
-serpent eliminer(serpent s){
+void suppression_queue(serpent s){
     serpent I = NULL;
     serpent S = NULL;
     while (s->suivant != NULL){
-        I = cons(I,s->coordonnees);
+        I = ajout_entete(I,s->coordonnees);
     }
     while (I != NULL){
-        S = cons(S,I->coordonnees);
+        S = ajout_entete(S,I->coordonnees);
     }
 return S;
 }
 
 /*deplacement*/
 
-serpent deplacement(int d, serpent s){ /*2,4,6 et 8 a remplacer pas las fleches*/
+void deplacement(direction d, serpent s){
     s = eliminer(s);
     position p;
     position c;
     c = s->coordonnees;
-    if (d==2){
+    if (d==BAS){
         p->x = c->x;
         p->y = c->y - 1;
         s = cons(s,p);
     }
-    if (d==8){
+    if (d==HAUT){
         p->x = c->x;
         p->y = c->y + 1;
         s = cons(s,p);
     }
-    if (d==6){
+    if (d==DROITE){
         p->x = c->x + 1;
         p->y = c->y;
         s = cons(s,p);
     }
-    if (d==4){
+    if (d==GAUCHE){
         p->x = c->x - 1;
         p->y = c->y;
         s = cons(s,p);
     }
-    return s;
 }
 
--- Code/snake.h
+++ Code/snake.h
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Snake */
+/* Snake */
+typedef struct position{
+  int x;
+  int y;
+} *position;
+
 typedef struct serpent {
   position coordonnees;
   struct maillon *suivant;
 } *serpent;
 
+
+typedef enum
+{
+  HAUT, BAS, GAUCHE, DROITE
+} direction;
+
 serpent cons(serpent s, position p);
 serpent eliminer(serpent s);
-serpent deplacement(int d, serpent s);
+void deplacement(direction d, serpent s);
