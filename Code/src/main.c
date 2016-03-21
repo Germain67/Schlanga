f9c6561d6a307cb2177d834be494e2c1e7d49846
Germain <germain_clauss@yahoo.fr>
2016-03-21 23:32:20 +0100

    Gestion des collisions

--- Code/src/main.c
+++ Code/src/main.c
@@ -9,7 +9,7 @@
 #include "test.h"
 #include "sdl_functions.h"
 #define FPS 30
-#define MOVE_TIME 500
+#define MOVE_TIME 200
 
 direction dir = DROITE;
 Uint32 lastMove = 0;
@@ -32,28 +32,34 @@ int main(int argc, char *argv[])
 
   if( c == '2') {
     initEcran(ecran);
-    printf("1) Test unitaire de la fonction displayPlateau et initMurs permettant l'affichage d'un plateau avec des murs \n");
-    printf("2) Test unitaire de la fonction collision \n");
-    printf("3) Test unitaire de la fonction addSerpentPlateau \n");
-    printf("4) Test unitaire de la fonction updateSerpentPlateau \n");
-    char d;
-    printf("Choix du menu : ");
-    scanf(" %c",&d);
-    printf("\n");
-    if ( d == '1') {
-    	test_displayPlateau_initMurs();
-    }
-    else if ( d == '2') {
-    	test_collision();
-    }
-    else if ( d == '3') {
-    	test_addSerpentPlateau();
-    }
-    else if ( d == '4') {
+    while(1){
+      printf("1) Test unitaire de la fonction displayPlateau et initMurs permettant l'affichage d'un plateau avec des murs \n");
+      printf("2) Test unitaire de la fonction collision \n");
+      printf("3) Test unitaire de la fonction addSerpentPlateau \n");
+      printf("4) Test unitaire de la fonction updateSerpentPlateau \n");
+      printf("5) Quitter \n");
+      char d;
+      printf("Choix du menu : ");
+      scanf(" %c",&d);
+      printf("\n");
+      if ( d == '1') {
+        test_displayPlateau_initMurs();
+      }
+      else if ( d == '2') {
+        test_collision();
+      }
+      else if ( d == '3') {
+        test_addSerpentPlateau();
+      }
+      else if ( d == '4') {
         test_updateSerpentPlateau();
-    }
-    else {
-	printf("Vous n'avez pas sélectionné un menu qui existe. \n");
+      }
+      else if (d == '5'){
+        return 0;
+      }
+      else {
+        printf("Vous n'avez pas sélectionné un menu qui existe. \n");
+      }
     }
   }
   else
@@ -98,7 +104,16 @@ int main(int argc, char *argv[])
       Uint32 tempsActuel = SDL_GetTicks();
       if (tempsActuel - lastMove > MOVE_TIME) /* Si 30 ms se sont écoulées */
       {
-        p = updateJeu(p, dir);
+        int etatPartie = 0;
+        p = updateJeu(p, dir, &etatPartie);
+        if(etatPartie == 1){
+          printf("Vous avez gagné !\n");
+          break;
+        }
+        else if(etatPartie == 2){
+          printf("Vous avez perdu !\n");
+          break;
+        }
         lastMove = tempsActuel; /* Le temps "actuel" devient le temps "precedent" pour nos futurs calculs */
       }
       if (tempsActuel - lastRefresh > (1000/FPS)) /* Si 30 ms se sont écoulées */
