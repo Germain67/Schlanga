f46a33bc02ac06903f98518c9730ed2b4370bf49
Germain <germain_clauss@yahoo.fr>
2016-04-15 14:50:04 +0200

    Petites modifs de l'IA

--- Code/src/IA.c
+++ Code/src/IA.c
@@ -18,26 +18,26 @@ direction aleatoire(serpent schlanga, plateau p) {
     pos_possible[0]=-1; pos_possible[1]=-1; pos_possible[2]=-1; pos_possible[3]=-1; // Initialisation à -1
     int l=0; // Compteur de directions possibles
     if (collision(p,BAS,schlanga) == 0){ 
-            pos_possible[l]=0; // BAS = 0
+      pos_possible[l]=0; // BAS = 0
 	    l++;
     }
     if (collision(p,HAUT,schlanga) == 0){ 
-            pos_possible[l]=1; // HAUT = 1
+      pos_possible[l]=1; // HAUT = 1
 	    l++;
     }
     if (collision(p,GAUCHE,schlanga) == 0){ 
-            pos_possible[l]=2; // GAUCHE = 2
+      pos_possible[l]=2; // GAUCHE = 2
 	    l++;
     }
     if (collision(p,DROITE,schlanga) == 0){ 
-            pos_possible[l]=3; // DROITE = 3
+      pos_possible[l]=3; // DROITE = 3
 	    l++;
     }
     if (l==0) { // Le schlanga ne peut plus se déplacer, il a perdu, il nous suffit de lui donner une direction aléatoire
 	    pos_possible[l]=0;
     }
 
-    srand(time(NULL));
+    //srand(time(NULL));
     int dir=rand()%l; // Nous avons l directions possibles, nous en choisissons une.
 
     if (pos_possible[dir]==0) {
@@ -56,19 +56,40 @@ direction aleatoire(serpent schlanga, plateau p) {
     return dir1;
 }
 
+direction int_to_dir(int i){
+	if(i == 0){
+		return HAUT;
+	}
+	else if(i == 1){
+		return BAS;
+	}
+	else if(i == 2){
+		return GAUCHE;
+	}
+	else{
+		return DROITE;
+	}
+}
+
 // IA défensif
 direction IA_defensif_direction(serpent schlanga, plateau p) {
 	int scan=4;	
 	int nb_case=0;
-	int count[4]; // count[0] = HAUT - count[1] = BAS - count[2] = GAUCHE - count[3] = DROITE
-	count[0]=-1; count[1]=-1; count[2]=-1; count[3]=-1;
+	int count[4]; 
 	direction dir;
 	int lar=p->largeur;
 	int hau=p->hauteur;
 	int y=schlanga->coordonnees->y;
 	int x=schlanga->coordonnees->x;
+	
 
 	int i,j;
+
+	//count[0] = HAUT - count[1] = BAS - count[2] = GAUCHE - count[3] = DROITE
+	count[0]=-1; 
+	count[1]=-1; 
+	count[2]=-1; 
+	count[3]=-1;
 
 	// Cas en haut
 	if (collision(p,HAUT,schlanga)==0) {
@@ -95,11 +116,12 @@ direction IA_defensif_direction(serpent schlanga, plateau p) {
 		}
 	}
 
-	y=schlanga->coordonnees->y;
-	x=schlanga->coordonnees->x;
+	
 
 	// Cas en bas
 	if (collision(p,BAS,schlanga)==0) {
+		y=schlanga->coordonnees->y;
+		x=schlanga->coordonnees->x;
 		for (i=0;i<scan;i++) {
 			y=y+1;
 			nb_case=scan-i-1; // Le pas de case à regarder à gauche et à droite.
@@ -123,11 +145,12 @@ direction IA_defensif_direction(serpent schlanga, plateau p) {
 		}
 	}
 
-	y=schlanga->coordonnees->y;
-	x=schlanga->coordonnees->x;
+
 
 	// Cas à gauche
-	if (collision(p,GAUCHE,schlanga)==0) {	
+	if (collision(p,GAUCHE,schlanga)==0) {
+		y=schlanga->coordonnees->y;
+		x=schlanga->coordonnees->x;	
 		for (i=0;i<scan;i++) {
 			nb_case=scan-i-1; // Le pas de case à regarder en haut et en bas.
 			x=x - 1; // On regarde à gauche
@@ -151,12 +174,10 @@ direction IA_defensif_direction(serpent schlanga, plateau p) {
 		}
 	}
 
-
-	y=schlanga->coordonnees->y;
-	x=schlanga->coordonnees->x;
-
 	// Cas à droite
 	if (collision(p,DROITE,schlanga)==0) {
+		y=schlanga->coordonnees->y;
+		x=schlanga->coordonnees->x;
 		for (i=0;i<scan;i++) {
 			nb_case=scan-i-1; // Le pas de case à regarder en haut et en bas.
 
@@ -181,19 +202,22 @@ direction IA_defensif_direction(serpent schlanga, plateau p) {
 		}
 	}
 
+	/* DEBUG
 	printf(" Haut : %d \n Bas : %d \n Gauche : %d \n Droite : %d \n \n", count[0], count[1], count[2], count[3]);
+	*/
 
-	if (count[0]==-1 && count[1]==-1 && count[2]==-1 && count[3]==-1) { // Le schlanga ne peut plus se déplacer, il a perdu, il nous suffit de lui donner une direction aléatoire où il va se suicider
-	dir=GAUCHE; 
+	if (count[0]==-1 && count[1]==-1 && count[2]==-1 && count[3]==-1) 
+	{ 
+		// Le schlanga ne peut plus se déplacer, il va se suicider
+		dir=GAUCHE; 
 	}
 	else {
-
 		// Récupérer le maximum
 		int max=0;
 		int max_atteint[4];
 		for (i=0;i<4;i++) { // Calcul du max
 			if (max < count[i]) {
-				max=count[i];
+				max = count[i];
 			}
 			max_atteint[i]=0; // On initialise max_atteint
 		}
@@ -206,22 +230,10 @@ direction IA_defensif_direction(serpent schlanga, plateau p) {
 			}
 		}
 		
-
 		srand(time(NULL));
 		int val=rand()%l;
 
-		if (max_atteint[val]==0) {
-			dir=HAUT;
-		}
-		if (max_atteint[val]==1) {
-			dir=BAS;
-		}
-		if (max_atteint[val]==2) {
-			dir=GAUCHE;
-		}
-		if (max_atteint[val]==3) {
-			dir=DROITE;
-		}
+		dir = int_to_dir(max_atteint[val]);
 	}
 	
 	return dir;
