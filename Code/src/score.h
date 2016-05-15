62ade7014f5945b69633ce5ec84829078b56b290
lise <lisediagne@gmail.com>
2016-05-15 15:21:56 +0200

    Affichage du score qui défile pendant le jeu et fenêtre gameover à la fin

---
+++ Code/src/score.h
@@ -0,0 +1,13 @@
+//
+// Created by lise on 14/05/16.
+//
+
+#ifndef SCHLANGA_SCORE_H
+#define SCHLANGA_SCORE_H
+
+
+void Gameover(int compteur, char* mess);
+
+int live_scores ();
+
+#endif //SCHLANGA_SCORE_H
